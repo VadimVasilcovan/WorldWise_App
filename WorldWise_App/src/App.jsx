f4e126a9474b459a8e5856f1f3cbcc30bb963287
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-02 17:55:50 +0300

    02.04.2025

--- WorldWise_App/src/App.jsx
+++ WorldWise_App/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Homepage from "./pages/Homepage";
+import PricingPage from "./pages/PricingPage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
-    <div>WorldWise</div>
-  )
+    <div>
+      
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<PricingPage />} />
+        <Route path='*' element={<PageNotFound/>}/>
+      </Routes>
+    </BrowserRouter>
+    </div>
+  );
 }
