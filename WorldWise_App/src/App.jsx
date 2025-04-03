ac996bd4e46dc71017ce37068f195264fb12fb4d
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-03 23:07:58 +0300

    03.04.2025

--- WorldWise_App/src/App.jsx
+++ WorldWise_App/src/App.jsx
@@ -2,27 +2,22 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
-import PricingPage from "./pages/PricingPage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Pricing from "./pages/Pricing";
 
 export default function App() {
   return (
-
     <div>
-      
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<PricingPage />} />
-        <Route path="app" element={<AppLayout/>} />
-        <Route path='*' element={<PageNotFound/>}/>
-      </Routes>
-    </BrowserRouter>
-
-
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- WorldWise_App/src/assets/components/PageNav.jsx
+++ WorldWise_App/src/assets/components/PageNav.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
 import styles from './PageNav.module.css'
+import Logo from './Logo';
 
 export default function PageNav() {
   return (
     <nav className={styles.nav}>
+        <Logo/>
         <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
+          
             <li>
                 <NavLink to='/pricing'>pricing</NavLink>
             </li>
             <li>
                 <NavLink to='/product'>Product</NavLink>
+            </li>
+            <li>
+                <NavLink to='/login'>Login</NavLink>
             </li>
         </ul>
     </nav>
