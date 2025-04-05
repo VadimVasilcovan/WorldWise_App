a1b711319732f3eac5e0952289385b5336b49824
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-05 13:12:24 +0300

    05.04.2025
    Nested Routes and Index Route

--- WorldWise_App/src/App.jsx
+++ WorldWise_App/src/App.jsx
@@ -16,7 +16,13 @@ export default function App() {
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
           <Route path="/login" element={<Login />} />
-          <Route path="app" element={<AppLayout />} />
+          <Route path="app" element={<AppLayout />}>
+            {/* index used for default path like "/" at Homepage*/}
+            <Route index element={<p>List of cities</p>} />{" "}
+            <Route path="cities" element={<p>List of cities</p>} />
+            <Route path="countries" element={<p>Countries</p>} />
+            <Route path="form" element={<p>Form</p>} />
+          </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
