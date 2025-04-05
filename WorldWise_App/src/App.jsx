0e0538b306ddc4c15ba81044e5b804dba243d247
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-05 14:05:56 +0300

    05.04.2025

--- WorldWise_App/src/App.jsx
+++ WorldWise_App/src/App.jsx
@@ -6,8 +6,30 @@ import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import Pricing from "./pages/Pricing";
+import CityList from "./assets/components/CityList";
+import { useState } from "react";
+import { useEffect } from "react";
 
+const BASE_URL = 'http://localhost:9000'
 export default function App() {
+  const [cities, setCities] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(function (){
+    async function fetchCities(){
+      try{
+        setIsLoading(true)
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      }catch{
+        alert('There was an error loading data...')
+      }finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[])
   return (
     <div>
       <BrowserRouter>
@@ -18,8 +40,8 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
             {/* index used for default path like "/" at Homepage*/}
-            <Route index element={<p>List of cities</p>} />{" "}
-            <Route path="cities" element={<p>List of cities</p>} />
+            <Route index element={<CityList/>} />
+            <Route path="cities" element={<CityList/>} />
             <Route path="countries" element={<p>Countries</p>} />
             <Route path="form" element={<p>Form</p>} />
           </Route>
---
+++ WorldWise_App/src/assets/components/CityList.jsx
@@ -0,0 +1,11 @@
+import styles from './CityList.module.css'
+
+import React from 'react'
+
+export default function CityList() {
+  return (
+    <ul className={styles.CityList}>
+        LIST
+    </ul>
+  )
+}
