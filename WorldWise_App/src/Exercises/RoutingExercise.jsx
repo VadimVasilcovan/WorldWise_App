238bd14626ebf416dc5ae90f31c6ecac89ac23da
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-07 12:46:29 +0300

    07.04.2025

---
+++ WorldWise_App/src/Exercises/RoutingExercise.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+const APITEST = 'https://potterapi-fedeperin.vercel.app/en/books'
+export default function RoutingExercise() {
+  return (
+    <div>
+        <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+        </BrowserRouter>
+    </div>
+  )
+}
---
+++ WorldWise_App/src/Exercises/pages/HomePageEx.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomePageEx() {
+  return (
+    <div>HomePageEx</div>
+  )
+}
