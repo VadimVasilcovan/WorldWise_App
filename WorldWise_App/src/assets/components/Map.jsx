7bfb7fac7a1f65d12462474c33ce686a38e69b0f
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-11 22:43:52 +0300

    11.04.2025

--- WorldWise_App/src/assets/components/Map.jsx
+++ WorldWise_App/src/assets/components/Map.jsx
@@ -12,7 +12,7 @@ export default function Map() {
   
   return (
     <div className={styles.mapContainer} >
-     <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={false} className={styles.map}>
+     <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
