e9d4bfe017950c174838fac962faef96c8c92251
VadimVasilcovan <152361519+VadimVasilcovan@users.noreply.github.com>
2025-04-12 17:30:18 +0300

    12.04.2025

--- WorldWise_App/src/assets/components/CityItem.jsx
+++ WorldWise_App/src/assets/components/CityItem.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCities } from "../contexts/CityContexts";
 
-
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
     day: "numeric",
@@ -12,19 +11,24 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 export default function CityItem({ city }) {
-  const {currentCity} = useCities();
-  
-  const { cityName, emoji, date, id, position} = city;
-  console.log(position)
+  const { currentCity } = useCities();
+
+  const { cityName, emoji, date, id, position } = city;
+  console.log(position);
   console.log(city);
-  console.log(currentCity)
+  console.log(currentCity);
   return (
-    <li >
-      <Link className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ''}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
-      <span className={styles.emoji}>{emoji}</span>
-      <h3 className={styles.name}>{cityName}</h3>
-      <time className={styles.date}>({formatDate(date)})</time>
-      <button className={styles.deleteBtn}>&times;</button>
+    <li>
+      <Link
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
+        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+      >
+        <span className={styles.emoji}>{emoji}</span>
+        <h3 className={styles.name}>{cityName}</h3>
+        <time className={styles.date}>({formatDate(date)})</time>
+        <button className={styles.deleteBtn}>&times;</button>
       </Link>
     </li>
   );
--- WorldWise_App/src/assets/components/Map.jsx
+++ WorldWise_App/src/assets/components/Map.jsx
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
 import styles from "./Map.module.css";
-import { useSearchParams, useNavigate,  } from "react-router-dom";
-import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet'
+import { useSearchParams, useNavigate } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "../contexts/CityContexts";
 
 export default function Map() {
-  const navigate = useNavigate()
-  const [mapPosition, setMapPosition] = useState([40, 0])
+  const navigate = useNavigate();
+  const { cities } = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
-  
+
   return (
-    <div className={styles.mapContainer} >
-     <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
-    <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-    />
-    <Marker position={mapPosition}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
-  </MapContainer>
+    <div className={styles.mapContainer}>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+            <Popup>
+             <span>{city.emojy}</span><span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
